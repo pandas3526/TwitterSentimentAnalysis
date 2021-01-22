c291bfa0b97b680121fe13d1c4376811f0b07f6f
Burcu İçen <burcuicen2000@gmail.com>
2021-01-22 18:36:30 +0300

    initial commit

--- Main.py
+++ Main.py
@@ -39,7 +39,7 @@ df = df.drop(['id', 'date', 'query', 'user_id'], axis=1)
 
 #In the sentiment column, there are 3 values, 0 indicating that the sentiment is negative,2 indicating that the sentiment is neutral, 4 indicates that sentiment is positive. That's why we change the values(0,4) to (Negative, Positive)
 
-lab_to_sentiment = {0:"Negative",2:"Neutral 4:"Positive"}
+lab_to_sentiment = {0:"Negative", 4:"Positive"}
 def label_decoder(label):
   return lab_to_sentiment[label]
 df.sentiment = df.sentiment.apply(lambda x: label_decoder(x))
@@ -166,5 +166,147 @@ embedding_layer = tf.keras.layers.Embedding(vocab_size,
                                           input_length=MAX_SEQUENCE_LENGTH,
                                           trainable=False) 
 print("Word embedding completed...")
+"""Now we will implement our model with our findings above. In our data there are some words feature in both Positive and Negative tweets,
+    This could be a problem in our learning model. That's why we use Sequence Models. For model architecture, we use
+	Embedding Layer - Generates Embedding Vector for each input sequence.
+	Conv1D Layer - Its using to convolve data into smaller feature vectors.
+	LSTM - Long Short Term Memory, its a variant of RNN which has memory state cell to learn the context of words which are at further along the text to carry contextual meaning rather than just neighbouring words as in case of RNN.
+	Dense - Fully Connected Layers for classification
+"""
+#importing necessary libraries
+
+from tensorflow.keras.layers import Conv1D, Bidirectional, LSTM, Dense, Input, Dropout
+from tensorflow.keras.layers import SpatialDropout1D
+
+#Building Sequence Models
+
+sequence_input = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32') #we use input as tokenized and encoded data as starter
+embedding_sequences = embedding_layer(sequence_input) #after that we fit our input to embedding layer we created above
+
+#Now we will implement layers to our embedded data by order below
+
+x = SpatialDropout1D(0.2)(embedding_sequences) #it drops entire 1D feature maps
+
+x = Conv1D(64, 5, activation='relu')(x) #to convolve data into smaller feature vectors.
+
+x = Bidirectional(LSTM(64, dropout=0.2, recurrent_dropout=0.2))(x) #we will use bidirectional LSTM to implement our model
+
+x = Dense(512, activation='relu')(x) #by adding dense layer, we feed all outputs from the previous layer to all its neurons, each neuron providing one output to the next layer. (This is enterance dense layer)
+
+x = Dropout(0.5)(x) #to prevent overfitting we use dropout layer(it sets input units to 0 with a frequency of rate at each step during training time)
+			#dropout layer can only be used on fully connected layers, it prevents overfitting by deactivating some neurons, Fine Tuning
+
+x = Dense(512, activation='relu')(x) #(This is exit dense layer)
+outputs = Dense(1, activation='sigmoid')(x) #final outputs
+model = tf.keras.Model(sequence_input, outputs) #fitting model
+
+"""We created our LSTM model, now we will implement Optimization Algorithm with callbacks and start training
+For optimization algorithm we will use Adam. We will use 2 callbacks. Callbacks are special functions which are called at the end of an epoch. The callbacks we used here:
+
+LRScheduler - It changes a Learning Rate at specfic epoch to achieve more improved result. In this notebook, the learning rate exponentionally decreases after remaining same for first 10 Epoch.
+ModelCheckPoint - It saves best model while training based on some metrics. Here, it saves the model with minimum Validity Loss.""" 
+
+from tensorflow.keras.optimizers import Adam  #Adam optimization algorithm
+from tensorflow.keras.callbacks import ReduceLROnPlateau #Callbacks
+
+model.compile(optimizer=Adam(learning_rate=LR), loss='binary_crossentropy', #Optimizing
+              metrics=['accuracy'])
+ReduceLROnPlateau = ReduceLROnPlateau(factor=0.1,	#Using callbacks
+                                     min_lr = 0.01,
+                                     monitor = 'val_loss',
+                                     verbose = 1)
+#We setted everything up, now we will start training
+print("Training on GPU...") if tf.config.list_physical_devices('GPU') else print("Training on CPU...")
+import time
+start = time.time()
+
+history = model.fit(x_train, y_train, batch_size=BATCH_SIZE, epochs=EPOCHS,
+                    validation_data=(x_test, y_test), callbacks=[ReduceLROnPlateau])
+
+print("Training completed...")
+stop = time.time()
+print("Training time: {}h".format((stop-start)/3600))
+
+"""We finished building our model and trained our model with the data. Now we will test the accuracy of our model. 
+Since we trained our model to determine the sentiment of a given sentence, we will test the program in accordance with 
+this purpose.
+"""
+
+SENTIMENT_THRESHOLDS = (0.4, 0.7) 
+"""The function below will be used to give meaning to our trained model with scores. Label will be determined by the comparison
+of score and thereshold values above. 
+Label is:
+    'Neutral' if 0.4<score<0.7 
+    'Negative' if score<=0.4 
+    'Positive' if score>=0.7      
+"""
+####################Model Evaluation####################
+"""Now that we have trained the model, we can evaluate its performance. 
+We will some evaluation metrics and techniques to test the model."""
+
+#Accuracy of our sentiment analysis model
+score = model.evaluate(x_test, y_test, batch_size=BATCH_SIZE)
+print()
+print("ACCURACY:",score[1])
+print("LOSS:",score[0])
+#Learning Curve of loss and accuracy of the model on each epoch.
+s, (at, al) = plt.subplots(2,1)
+at.plot(history.history['accuracy'], c= 'b')
+at.plot(history.history['val_accuracy'], c='r')
+at.set_title('model accuracy')
+at.set_ylabel('accuracy')
+at.set_xlabel('epoch')
+at.legend(['LSTM_train', 'LSTM_val'], loc='upper left')
+
+al.plot(history.history['loss'], c='m')
+al.plot(history.history['val_loss'], c='c')
+al.set_title('model loss')
+al.set_ylabel('loss')
+al.set_xlabel('epoch')
+al.legend(['train', 'val'], loc = 'upper left')
+plt.close()
+
+####################Model Testing####################
+"""In this part, To test accuracy of our model, we will send some sentences as user input to our trained model for testing."""
+
+def decode_sentiment(score, include_neutral=True):
+    label = 'Neutral'
+    if score <= SENTIMENT_THRESHOLDS[0]:
+        label = 'Negative'
+    elif score >= SENTIMENT_THRESHOLDS[1]:
+        label = "Positive"
+
+    return label
+    
+"""This function takes text as input and determine if label of the text is Positive, Negative or Neutral"""    
+def predict(text):
+    # Tokenize text
+    x_test = pad_sequences(tokenizer.texts_to_sequences([text]), maxlen=MAX_SEQUENCE_LENGTH)
+    # Predict score
+    score = model.predict([x_test])[0]
+    # Decode sentiment
+    label = decode_sentiment(score)
+
+    return {"label": label, "score": float(score)}
+
+scores = model.predict(x_test, verbose=1, batch_size=10000) 
+
+y_pred_1d = [decode_sentiment(score) for score in scores]
+
+#User interface to use the program; It takes a text input from the user and print its label
+
+def start_program():
+    userInput=""
+    while(userInput!="q"):
+        userInput=input("Enter a sentence: \n Press q to quit program ")
+        if(userInput=="q"):
+            time.sleep(2)
+            print("Exiting program...")
+        else:    
+            print(predict(userInput))
+
+start_program()      
+
+      
 
 
